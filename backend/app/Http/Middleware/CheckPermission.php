a9714476a52de5f378ec5fe36a410b13c66ae0f5
Николай Трифонов <gynter37@gmail.com>
2025-04-07 23:52:50 +0300

    feat: добавлен middleware

---
+++ backend/app/Http/Middleware/CheckPermission.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Models\User;
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Tymon\JWTAuth\Facades\JWTAuth;
+
+class CheckPermission
+{
+    public function handle(Request $request, Closure $next, string $permission): Response
+    {
+        try {
+            $userId = JWTAuth::parseToken()->getPayload()->get('sub');
+
+            $user = User::query()->find($userId);
+
+            if (!$user) {
+                return response()->json([
+                    'status' => 'error',
+                    'message' => 'User not found'
+                ], 404);
+            }
+//dump($permission);
+//            dd($user->role->permissions()->pluck('permissions.code')->toArray(), );
+            if (! $user->hasPermission($permission)) {
+                return response()->json([
+                    'status' => 'error',
+                    'message' => 'Forbidden'
+                ], 403);
+            }
+
+            return $next($request);
+
+        } catch (\Exception $e) {
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Unauthorized'
+            ], 401);
+        }
+    }
+}
